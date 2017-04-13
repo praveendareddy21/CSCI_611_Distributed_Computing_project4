352ebccbd8c08d694258652dca18d8a5ec75505b
praveendareddy21 <praveendareddy@gmail.com>
2017-04-13 10:27:10 -0700

    added server and client bare shells for testing game

---
+++ server.cpp
@@ -0,0 +1,9 @@
+#ifndef SHM_CONSTANTS
+#define SHM_CONSTANTS
+#define  SHM_SM_NAME "/PD_S_semaphore"
+#define  SHM_NAME "/PD_S_SharedMemory"
+#define MSG_QUEUE_PREFIX "/PD_S_MSG_QUEUE_P"
+#define IS_CLIENT 0
+#endif
+
+#include"test_main.cpp"
