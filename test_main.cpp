d3ce3ed25816abbc2e2788adde24d6c44fd71060
praveendareddy21 <praveendareddy@gmail.com>
2017-05-07 15:22:18 -0700

    removed IS_client setting and reset mymap to original

--- test_main.cpp
+++ test_main.cpp
@@ -36,7 +36,6 @@ using namespace std;
 #define  SHM_SM_NAME "/PD_semaphore"
 #define  SHM_NAME "/PD_SharedMemory"
 #define MSG_QUEUE_PREFIX "/PD_MSG_QUEUE_P"
-#define IS_CLIENT -1
 #define PORT "42425"  //change this # between 2000-65k before using
 #define PORT1 "42426"  //change this # between 2000-65k before using
 #endif
@@ -1319,7 +1318,7 @@ int main(int argc, char *argv[])
   unsigned char * mp; //map pointer
   vector<vector< char > > mapVector;
 
-  if(IS_CLIENT == -1 && argc == 2){ // ip to connect daemon server
+  if(argc == 2){ // ip to connect daemon server
     ip_address = argv[1];
     inClientNode = true;
     shm_sem = sem_open(SHM_SM_NAME ,O_RDWR,S_IRUSR|S_IWUSR,1);
@@ -1338,7 +1337,7 @@ int main(int argc, char *argv[])
       }
     }
   }
-  else if(IS_CLIENT == -1 && argc == 1){ // ip to connect daemon server
+  else if(argc == 1){ // ip to connect daemon server
     inServerNode = true;
   }
 
